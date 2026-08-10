7ae38d4d7f935ab157a96cdce6f45a5d78537c35
Marcos Alexandre <marcosvini864@gmail.com>
2026-08-10 15:31:25 -0300

    fix: corrige import supabase no DiagnosticoTributario

--- src/pages/DiagnosticoTributario.jsx
+++ src/pages/DiagnosticoTributario.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
-import { supabase } from '../../supabaseClient';
+import { supabase } from '../supabase';
 import { parseXMLNFe, agruparPorCompetencia } from '../utils/parseXMLNFe'
 import MotorInteligenciaTributaria from '../motor/MotorInteligenciaTributaria'
 import FunctionalPageTemplate, { DS, PageHeader, KpiCard, ValorDestaque, ContentCard, ListaDocumentos, BtnPrimario, BtnSecundario } from '../FunctionalPageTemplate'
